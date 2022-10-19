899d4a46edbb6d56838af028f52afe4a533e4011
Jovann Contreras <jcontreras@codestack.co>
2022-10-19 12:11:11 -0700

    Got vaildation for odd or even

--- Program.cs
+++ Program.cs
@@ -1,3 +1,59 @@
 ﻿//Jovann Contreras
 //Date 10/19/2022
 //Getting the user to type in a number to check if it is odd or even.
+
+Console.Clear();
+string playAgain = "yes";
+while (playAgain == "yes")
+{
+
+    int num1 = 0;
+    bool numberOne = false;
+
+    Console.WriteLine("Welcome to odd or even");
+
+    while (!numberOne)
+    {
+        Console.Write("Enter in a number to see if it is odd or even: ");
+        numberOne = Int32.TryParse(Console.ReadLine(), out num1);
+
+        if (!numberOne)
+        {
+            Console.WriteLine("Invalid please enter in a number");
+        }
+    }
+
+    if (num1 % 2 == 0)
+    {
+        Console.WriteLine($"{num1} is even");
+    }
+    else
+    {
+        Console.WriteLine($"{num1} is odd");
+    }
+
+    Console.Write("Would you like to try again? Type in Yes to retry or No to quit: ");
+
+    string playAgainInput = "Nothing";
+
+    while (playAgainInput != "yes" || playAgainInput != "no")
+    {
+        playAgainInput = Console.ReadLine().ToLower();
+        if (playAgainInput == playAgain)
+        {
+            break;
+        }
+        else if (playAgainInput == "no")
+        {
+            Console.WriteLine("Thanks for playing!");
+            playAgain = playAgainInput;
+            break;
+
+        }
+        else
+        {
+            Console.WriteLine("Invalid Input");
+            playAgainInput = "invalid";
+        }
+    }
+}
