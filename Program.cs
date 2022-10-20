84bdf2b32e680288f734c45ea9fe99c2f439058a
Jovann Contreras <jcontreras@codestack.co>
2022-10-20 14:57:50 -0700

    comment

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿
+﻿// Jovann Contreras
+// Date 10/19/2022
+// Getting the user to type in a number to check if it is odd or even.
+
 Console.Clear();
 string playAgain = "yes";
 while (playAgain == "yes")
